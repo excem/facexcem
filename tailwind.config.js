6c79e7f1941a6efe84753b43d178942477856eff
excem <efgymnqayths@gmail.com>
2025-08-26 17:48:09 +0200

    feat(FIRST COMMIT)
    FE

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+      './src/components/fe/**/*.{js,ts,jsx,tsx}', // adjust if different
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
